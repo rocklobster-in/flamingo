40995b974ac5fbccb578ad9c9d4d0a3070f92c2c
Takayuki Miyoshi <takayukister@gmail.com>
2021-12-13 18:07:08 +0900

    Update cron.php
    #84

--- includes/cron.php
+++ includes/cron.php
@@ -1,23 +1,17 @@
 <?php
-
 /**
- * The file that defines the cron job functionality
+ * Cron job functionality
  *
  * @since 2.1
- *
  */
 
 
-// call when WP loads
 add_action( 'wp', 'flamingo_schedule_activation', 10, 0 );
 
 /**
- * Create schedule event for cron job, if its already not exists
+ * Creates a scheduled event, if it does not exist.
  *
  * @since 2.1
- *
- * @see wp_next_scheduled(), wp_schedule_event()
- *
  */
 function flamingo_schedule_activation() {
 	if ( ! wp_next_scheduled( 'flamingo_daily_cron_job' ) ) {
@@ -26,40 +20,30 @@ function flamingo_schedule_activation() {
 }
 
 
-// deactivate cron job on deactivation of the plugin on plugin's deactivation
+// Unscheduling cron jobs on plugin deactivation.
 register_deactivation_hook( FLAMINGO_PLUGIN, 'flamingo_schedule_deactivate' );
 
 /**
- * Function to deactivate the cron job
+ * Unschedules cron jobs.
  *
  * @since 2.1
- *
- * @see wp_next_scheduled(), wp_unschedule_event()
- *
  */
 function flamingo_schedule_deactivate() {
 
-	// when the last event was scheduled
+	// Timestamp of when the last event was scheduled
 	$timestamp = wp_next_scheduled( 'flamingo_daily_cron_job' );
 
-	// unschedule previous event if any
 	wp_unschedule_event( $timestamp, 'flamingo_daily_cron_job' );
 }
 
 
-// hook flamingo_schedule_function to schedule event
 add_action( 'flamingo_daily_cron_job', 'flamingo_schedule_function', 10, 0 );
 
 /**
- * Function to run for cron job
+ * The cron job.
  *
  * @since 2.1
- *
- * @see flamingo_schedule_move_trash()
- *
  */
 function flamingo_schedule_function() {
-
-	// run function move spam to trash
 	flamingo_schedule_move_trash();
 }
