cf40fa517482f933da696ace1fdc3ee67361b148
Takayuki Miyoshi <takayukister@gmail.com>
2025-07-01 17:19:33 +0900

    Update admin-functions.php
    #29

--- admin/admin-functions.php
+++ admin/admin-functions.php
@@ -1,18 +1,15 @@
 <?php
 
 function flamingo_current_action() {
-	if ( isset( $_REQUEST['delete_all'] )
-	or isset( $_REQUEST['delete_all2'] ) ) {
+	if ( isset( $_REQUEST['delete_all'] ) or isset( $_REQUEST['delete_all2'] ) ) {
 		return 'delete_all';
 	}
 
-	if ( isset( $_REQUEST['action'] )
-	and -1 != $_REQUEST['action'] ) {
+	if ( isset( $_REQUEST['action'] ) and -1 !== $_REQUEST['action'] ) {
 		return $_REQUEST['action'];
 	}
 
-	if ( isset( $_REQUEST['action2'] )
-	and -1 != $_REQUEST['action2'] ) {
+	if ( isset( $_REQUEST['action2'] ) and -1 !== $_REQUEST['action2'] ) {
 		return $_REQUEST['action2'];
 	}
 
