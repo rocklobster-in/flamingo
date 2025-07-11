d6a73782a02db0facae4dc6ac173e93a9e222575
Takayuki Miyoshi <takayukister@gmail.com>
2025-07-11 16:43:40 +0900

    Introduce flamingo_hourly_cron_job hook
    #149

--- includes/cron.php
+++ includes/cron.php
@@ -14,9 +14,11 @@ add_action( 'admin_init', 'flamingo_schedule_activation', 10, 0 );
  * @since 2.1
  */
 function flamingo_schedule_activation() {
-	if ( ! wp_next_scheduled( 'flamingo_daily_cron_job' ) ) {
-		wp_schedule_event( time(), 'daily', 'flamingo_daily_cron_job' );
+	if ( ! wp_next_scheduled( 'flamingo_hourly_cron_job' ) ) {
+		wp_schedule_event( time(), 'hourly', 'flamingo_hourly_cron_job' );
 	}
+
+	wp_clear_scheduled_hook( 'flamingo_daily_cron_job' );
 }
 
 
@@ -29,15 +31,12 @@ register_deactivation_hook( FLAMINGO_PLUGIN, 'flamingo_schedule_deactivate' );
  * @since 2.1
  */
 function flamingo_schedule_deactivate() {
-
-	// Timestamp of when the last event was scheduled
-	$timestamp = wp_next_scheduled( 'flamingo_daily_cron_job' );
-
-	wp_unschedule_event( $timestamp, 'flamingo_daily_cron_job' );
+	wp_clear_scheduled_hook( 'flamingo_hourly_cron_job' );
+	wp_clear_scheduled_hook( 'flamingo_daily_cron_job' );
 }
 
 
-add_action( 'flamingo_daily_cron_job', 'flamingo_schedule_function', 10, 0 );
+add_action( 'flamingo_hourly_cron_job', 'flamingo_schedule_function', 10, 0 );
 
 /**
  * The cron job.
